964606d574012b7de0292ef4cc6835a9ada8555f
ajalaj <aditi.jalaj@gmail.com>
2021-05-24 20:39:18 +0530

    fetching trackList

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -2,13 +2,13 @@ import {Song} from '../context'
 import {useContext} from 'react'
 
 const Tracks = () => {
-    const tracksFetched=useContext(Song) 
+    const value=useContext(Song) 
     //const API_KEY = process.env.REACT_APP_API_KEY;
     
     return ( 
         <>
         {
-            
+            console.log(value)
         }
         </>
          );
--- src/context.js
+++ src/context.js
@@ -1,4 +1,4 @@
-import {createContext, useEffect } from 'react';
+import {createContext, useEffect, useState } from 'react';
 import Container from './components/Container';
 import Tracks from './components/Tracks'
 import axios from 'axios'
@@ -12,19 +12,22 @@ const Song= createContext()
 
 //e68b24457aa707447caae1c9f7b56a10
 
-let tracksFetched
-
 const Context = (props) => {
-    useEffect(()=>{
-        axios
-        .get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=e68b24457aa707447caae1c9f7b56a10`)
-    .then((res)=>console.log(res.data))
-    .catch((err)=>console.log(err))
     
+const [trackList,setTrackList]=useState([])
+
+
+    useEffect(()=>{
+    axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=e68b24457aa707447caae1c9f7b56a10`)
+    .then((res)=>{
+        console.log(res.data.message.body.track_list)
+        setTrackList(res.data.message.body.track_list)
+    })
+    .catch((err)=>console.log(err))
     })
 
     return ( 
-        <Song.Provider>
+        <Song.Provider value={trackList}>
         {props.children}
         </Song.Provider>
      );
