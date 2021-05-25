f2439b369cf8009b3c6aede6f6ccc7bc8667aea7
ajalaj <aditi.jalaj@gmail.com>
2021-05-25 11:28:23 +0530

    fixed the api call

--- src/context.js
+++ src/context.js
@@ -4,27 +4,22 @@ import Tracks from './components/Tracks'
 import axios from 'axios'
 
 const Song= createContext()
-// const tracks=[
-//         {track:{trackName:"ADI"}},
-//         {track:{trackName:"Jal"}}
-   
-// ]
 
 //e68b24457aa707447caae1c9f7b56a10
 
 const Context = (props) => {
     
-const [trackList,setTrackList]=useState([])
+let [trackList,setTrackList]=useState([])
 
 
     useEffect(()=>{
     axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=e68b24457aa707447caae1c9f7b56a10`)
     .then((res)=>{
-        console.log(res.data.message.body.track_list)
+        //console.log(res.data.message.body.track_list)
         setTrackList(res.data.message.body.track_list)
     })
     .catch((err)=>console.log(err))
-    })
+    },[])
 
     return ( 
         <Song.Provider value={trackList}>
