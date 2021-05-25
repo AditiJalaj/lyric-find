7ce480110b7384a0b0a563af6ec8498d229d94dd
ajalaj <aditi.jalaj@gmail.com>
2021-05-25 11:28:31 +0530

    fixed the api call

--- src/components/Tracks.js
+++ src/components/Tracks.js
@@ -3,8 +3,7 @@ import {useContext} from 'react'
 
 const Tracks = () => {
     const value=useContext(Song) 
-    //const API_KEY = process.env.REACT_APP_API_KEY;
-    
+  
     return ( 
         <>
         {
--- src/context.js
+++ src/context.js
@@ -5,24 +5,29 @@ import axios from 'axios'
 
 const Song= createContext()
 
+//https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=e68b24457aa707447caae1c9f7b56a10
 //e68b24457aa707447caae1c9f7b56a10
+//https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=5&country=it&f_has_lyrics=1&apikey=e68b24457aa707447caae1c9f7b56a10
 
 const Context = (props) => {
     
 let [trackList,setTrackList]=useState([])
 
+const [heading,setHeading]=useState("")
 
     useEffect(()=>{
-    axios.get(`https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&f_has_lyrics=1&apikey=e68b24457aa707447caae1c9f7b56a10`)
+    axios.get( `https://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/chart.tracks.get?chart_name=top&page=1&page_size=10&country=in&f_has_lyrics=1&apikey=e68b24457aa707447caae1c9f7b56a10`)
     .then((res)=>{
-        //console.log(res.data.message.body.track_list)
+       console.log(res.data)
+       console.log("use effect ran")
         setTrackList(res.data.message.body.track_list)
+        setHeading("Top 10 Tracks")
     })
     .catch((err)=>console.log(err))
     },[])
 
     return ( 
-        <Song.Provider value={trackList}>
+        <Song.Provider value={trackList,heading}>
         {props.children}
         </Song.Provider>
      );
