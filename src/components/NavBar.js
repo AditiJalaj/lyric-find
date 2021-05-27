1f749241ae80d6057aa894d2a40acd8d3e3b44d2
ajalaj <aditi.jalaj@gmail.com>
2021-05-27 18:53:20 +0530

    added request access to demo server note

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,7 +1,18 @@
 const NavBar = () => {
-    return ( <nav className="navbar">
+    return ( 
+        <div><nav className="navbar">
         Lyric-Find
-        </nav> );
+        </nav> 
+        <div className="info">
+        <p > If you're trying this site for the first time 
+        chances are there might be a 403 
+        error from the API (check console), click on {" "}
+        <a style={{color:"rgb(178, 198, 235)"}} href="https://cors-anywhere.herokuapp.com/corsdemo">
+        Request temporary access</a>  to continue
+          </p>
+          </div>
+          </div>
+        );
 }
  
 export default NavBar;
